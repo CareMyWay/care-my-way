637bd524ca1e9baffd1fc23cfb8befe69080ece5
Natalie <natalie.yeung16@gmail.com>
2025-05-14 14:57:05 -0600

    Complete navbar with auth functionality

--- src/components/navbars/navbar.tsx
+++ src/components/navbars/navbar.tsx
@@ -13,6 +13,7 @@ export default function NavBar({ isSignedIn }: { isSignedIn: boolean }) {
   const [, startTransition] = useTransition();
 
   const router = useRouter();
+
   useEffect(() => {
     const hubListenerCancel = Hub.listen("auth", (data) => {
       switch (data.payload.event) {
@@ -23,7 +24,6 @@ export default function NavBar({ isSignedIn }: { isSignedIn: boolean }) {
           break;
         case "signedOut":
           setAuthCheck(false);
-
           startTransition(() => router.push("/"));
           startTransition(() => router.refresh());
           break;
@@ -33,23 +33,13 @@ export default function NavBar({ isSignedIn }: { isSignedIn: boolean }) {
     return () => hubListenerCancel();
   }, [router]);
 
-  const signOutSignIn = async () => {
-    if (authCheck) {
-      await signOut();
-    } else {
-      router.push("/signin");
-    }
+  const handleSignOut = async () => {
+    await signOut();
   };
+
   const defaultRoutes = [
-    {
-      href: "/",
-      label: "Home",
-    },
-    {
-      href: "/add",
-      label: "Add Title",
-      loggedIn: true,
-    },
+    { href: "/", label: "Home" },
+    { href: "/add", label: "Add Title", loggedIn: true },
   ];
 
   const routes = defaultRoutes.filter(
@@ -62,7 +52,7 @@ export default function NavBar({ isSignedIn }: { isSignedIn: boolean }) {
         direction="row"
         justifyContent="space-between"
         alignItems="center"
-        padding={"1rem"}
+        padding="1rem"
       >
         <Flex as="nav" alignItems="center" gap="3rem" margin="0 2rem">
           {routes.map((route) => (
@@ -71,16 +61,37 @@ export default function NavBar({ isSignedIn }: { isSignedIn: boolean }) {
             </Link>
           ))}
         </Flex>
-        <Button
-          variation="primary"
-          borderRadius="2rem"
-          className="mr-4"
-          onClick={signOutSignIn}
-        >
-          {authCheck ? "Sign Out" : "Sign In"}
-        </Button>
+
+        <Flex gap="1rem" alignItems="center">
+          {authCheck ? (
+            <Button
+              variation="primary"
+              borderRadius="2rem"
+              onClick={handleSignOut}
+            >
+              Sign Out
+            </Button>
+          ) : (
+            <>
+              <Button
+                variation="link"
+                borderRadius="2rem"
+                onClick={() => router.push("/sign-in")}
+              >
+                Sign In
+              </Button>
+              <Button
+                variation="primary"
+                borderRadius="2rem"
+                onClick={() => router.push("/sign-up")}
+              >
+                Sign Up
+              </Button>
+            </>
+          )}
+        </Flex>
       </Flex>
-      <Divider size="small"></Divider>
+      <Divider size="small" />
     </>
   );
 }
--- src/utils/amplify-server-utils.ts
+++ src/utils/amplify-server-utils.ts
@@ -1,24 +1,38 @@
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
-import { getCurrentUser } from "aws-amplify/auth";
- 
+import { getCurrentUser } from "aws-amplify/auth/server";
+// eslint-disable-next-line no-restricted-imports
 import config from "../../amplify_outputs.json";
 import { cookies } from "next/headers";
 
+//use runWithAmplifyServerContext to call Amplify APIs within isolated request contexts - keeping it within the server
+// and not leaking to the client
 export const { runWithAmplifyServerContext } = createServerRunner({
-   
   config,
 });
 
 //check if user is logged in
 
-export async function GetAuthCurrentUserServer() {
-  try {
-    const currentUser = await runWithAmplifyServerContext({
-      nextServerContext: { cookies },
-      operation: (context) => getCurrentUser(context),
-    });
-    return currentUser;
-  } catch (error) {
-    console.log(error);
-  }
-}
+export const isAuthenticated = async () =>
+  await runWithAmplifyServerContext({
+    nextServerContext: { cookies },
+    async operation(contextSpec) {
+      try {
+        const user = await getCurrentUser(contextSpec);
+        return !!user;
+      } catch (error) {
+        return false;
+      }
+    },
+  });
+
+// export async function GetAuthCurrentUserServer() {
+//   try {
+//     const currentUser = await runWithAmplifyServerContext({
+//       nextServerContext: { cookies },
+//       operation: (context) => getCurrentUser(context),
+//     });
+//     return currentUser;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
