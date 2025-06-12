ef40ad36714dc1652015a6aeca480949cc345bfc
Natalie <natalie.yeung16@gmail.com>
2025-06-11 18:53:40 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,33 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    ignores: ["node_modules", "dist", ".next"],
+
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+    },
+
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      ...require("eslint-plugin-react").configs.recommended.rules,
+      ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
+      ...require("eslint-plugin-react-hooks").configs.recommended.rules,
+    },
+  },
   ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
