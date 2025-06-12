75796ae74cefb5c45dc30cba1c07ff49f985fd7a
Natalie <natalie.yeung16@gmail.com>
2025-06-11 19:59:18 -0600

    Simplify eslint.config.mjs, update eslint.yml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,50 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Use dynamic imports instead of require()
-const [
-  typescriptEslintPlugin,
-  reactPlugin,
-  reactHooksPlugin,
-  typescriptParser,
-] = await Promise.all([
-  import("@typescript-eslint/eslint-plugin"),
-  import("eslint-plugin-react"),
-  import("eslint-plugin-react-hooks"),
-  import("@typescript-eslint/parser"),
-]);
-
 const eslintConfig = [
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     ignores: ["node_modules", "dist", ".next"],
-    plugins: {
-      "@typescript-eslint": typescriptEslintPlugin.default,
-      react: reactPlugin.default,
-      "react-hooks": reactHooksPlugin.default,
-    },
-    languageOptions: {
-      parser: typescriptParser.default,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...typescriptEslintPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-    },
   },
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "next",
-    "prettier"
-  ),
+
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next", "prettier"],
+  }),
 ];
 
 export default eslintConfig;
