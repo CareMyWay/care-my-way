66f82c2a969a8cb7b1734594873ae8f6f860de13
benferaer <benferaer@gmail.com>
2025-07-09 19:21:38 -0600

    Booking modal working with dynamoDB

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+=======
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+>>>>>>> 70c4d7c (Booking modal working with dynamoDB)
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -33,7 +41,11 @@ const buttonVariants = cva(
       size: "default",
     },
   }
+<<<<<<< HEAD
 );
+=======
+)
+>>>>>>> 70c4d7c (Booking modal working with dynamoDB)
 
 function Button({
   className,
@@ -45,7 +57,11 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
+<<<<<<< HEAD
   const Comp = asChild ? Slot : "button";
+=======
+  const Comp = asChild ? Slot : "button"
+>>>>>>> 70c4d7c (Booking modal working with dynamoDB)
 
   return (
     <Comp
@@ -53,7 +69,14 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
+<<<<<<< HEAD
   );
 }
 
 export { Button, buttonVariants };
+=======
+  )
+}
+
+export { Button, buttonVariants }
+>>>>>>> 70c4d7c (Booking modal working with dynamoDB)
