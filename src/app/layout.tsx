29f3320a137e373fbe356d69001210b40e5e6409
benferaer <benferaer@gmail.com>
2025-07-14 21:06:52 -0600

    removed unused code for stripe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,9 +42,7 @@ export default async function RootLayout({
 
         <AuthProvider>
           <ConfigureAmplifyClientSide />
-          <StripeProvider>
           {children}
-          </StripeProvider>
         </AuthProvider>
       </body>
     </html>
