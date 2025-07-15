da0575dda9c66bbcac5aaeb922b142a200a19aff
Natalie <natalie.yeung16@gmail.com>
2025-07-14 23:29:15 -0600

    Add role based authorization on user account pages

---
+++ src/app/client-dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { getCurrentUserServer } from "@/utils/amplify-server-utils";
+import { getUserProfile } from "@/actions/getUserProfile";
+import HomeDashPage from "@/components/client-dashboard-ui/home-dash-page";
+
+export default async function ClientDashboardPage() {
+  const user = await getCurrentUserServer();
+  const userProfile = await getUserProfile(user.userId);
+
+  if (!userProfile) return <p>User not found</p>;
+
+  return (
+    <>
+      <p>
+        <strong>Email:</strong> {userProfile.email}
+      </p>
+      <p>
+        <strong>User Type:</strong> {userProfile.userType}
+      </p>
+      <HomeDashPage />
+    </>
+  );
+}
---
+++ src/components/layouts/provider-dashboard-layout.tsx
@@ -0,0 +1,38 @@
+// components/layouts/ProviderDashboardLayout.tsx
+import {
+  getCurrentUserServer,
+  checkIsInGroup,
+} from "@/utils/amplify-server-utils";
+import { getUserProfile } from "@/actions/getUserProfile";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import NavBar from "@/components/nav-bars/navbar";
+import { SidebarNav } from "../provider-dashboard-ui/careprovider-sidenav";
+export default async function ProviderDashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const isProvider = await checkIsInGroup("Provider");
+  if (!isProvider) redirect("/not-found");
+
+  const currentUser = await getCurrentUserServer();
+  const userProfileData = await getUserProfile(currentUser.userId);
+
+  if (!userProfileData) return <p>User not found</p>;
+
+  return (
+    <div className="min-h-screen flex bg-dashboard-bg font-manrope">
+      <aside className="w-64 h-screen fixed top-0 left-0 z-20 bg-white border-r border-gray-200">
+        <SidebarNav />
+      </aside>
+
+      <div className="ml-64 flex flex-col min-h-screen w-full">
+        <header className="w-full border-b border-gray-200 bg-white z-10">
+          <NavBar />
+        </header>
+        <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+}
