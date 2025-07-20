b76487aeb1a791c6906c16a41f8ad2dd629d0a92
Nathan-Gui <nathan.std@icloud.com>
2025-07-19 19:32:28 -0600

    to-do labels and bug fixing

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -8,43 +8,6 @@ import MarketplaceFilter from "@/components/marketplace/filter";
 import {fetchProviders} from "@/actions/fetchProviderMarketPlace";
 import Loading from "../loading";
 
-const demo_data = [
-  {
-    name: "Nina Nguyen",
-    title: "Health Care Aide",
-    location: "Calgary, AB",
-    experience: "5+ years",
-    testimonials: 10,
-    languages: ["English", "French", "Urdu"],
-    services: ["Dementia Care", "Personal Care", "Health Monitoring"],
-    hourlyRate: 20,
-    imageSrc: "/images/home/meet-providers/person-placeholder-1.png",
-  },
-  {
-    name: "Kenji Yamamoto",
-    title: "Live-in Caregiver",
-    location: "Calgary, AB",
-    experience: "2+ years",
-    testimonials: 18,
-    languages: ["English", "French", "Urdu"],
-    services: ["Housekeeping", "Companionship", "Mobility Assistance"],
-    hourlyRate: 31,
-    imageSrc: "/images/home/meet-providers/person-placeholder-2.jpg",
-  },
-  {
-    name: "Jamal Osborne",
-    title: "Registered Nurse",
-    location: "Calgary, AB",
-    experience: "10+ years",
-    testimonials: 5,
-    languages: ["English", "French", "Urdu"],
-    services: ["Wound Care", "Medication Management", "Health Education"],
-    hourlyRate: 56,
-    imageSrc: "/images/home/meet-providers/person-placeholder-6.jpg",
-  },
-];
-console.info("demo_data", demo_data);
-
 export interface Provider {
   name: string;
   title: string;
