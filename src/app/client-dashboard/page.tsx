d33af9a7743b18cd8307899bd48e7bfa94e27399
Natalie <natalie.yeung16@gmail.com>
2025-07-21 19:21:28 -0600

    Fix UI for dashboard and profile

--- src/app/client-dashboard/page.tsx
+++ src/app/client-dashboard/page.tsx
@@ -3,6 +3,11 @@ import { getUserProfile } from "@/actions/getUserProfile";
 import HomeDashPage from "@/components/client-dashboard/home-dash-page";
 
 export default async function ClientDashboardPage() {
+  const user = await getCurrentUserServer();
+  const userProfile = await getUserProfile(user.userId);
+
+  if (!userProfile) return <p>User not found</p>;
+
   return (
     <>
       <HomeDashPage />
