9d7a0e4693cd447a7e347fbddf0f79af77db1f24
Ashleen Sidhu <Ssidhuash4@gmail.com>
2025-08-08 11:43:10 -0600

    update api

--- src/app/api/confirm-payment/route.ts
+++ src/app/api/confirm-payment/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
     }
 
     const updateCommand = new UpdateItemCommand({
-      TableName: "Booking-lp62gakperctdporhcqw5fsfxi-NONE",
+      TableName: "Booking-iupg6yuovreq3mz2yzmuqz2epe-NONE",
       Key: {
         id: { S: bookingId },
       },
