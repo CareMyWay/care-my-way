656cb624b8b0da0e38eda30e3435fcf6ecd76621
benferaer <benferaer@gmail.com>
2025-11-06 17:20:14 -0700

    Update hero-section.tsx

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
               style={{ textShadow: "2px 2px 20px rgba(255, 255, 255, 0.8)" }}
               className=" text-darkest-green font-bold text-[35px] md:text-h2-size lg:text-h1-size w-3/5"
             >
-              {t("Needing healthcare help at home can happen when you least expect it.")}
+              {t("Needing healthcare help at home can happen when you least expect it")}
             </h1>
             <OrangeButton variant="route" href="/marketplace" className="w-fit">
               {t("Find a Healthcare Provider")}
