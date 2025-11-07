31b860b067b7067ad66642168243593a1f02b889
Natalie <natalie.yeung16@gmail.com>
2025-11-06 17:33:07 -0700

    Create dev branch to trigger eslint

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import OrangeButton from "../buttons/orange-button";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 const HeroSection = () => {
   const { t } = useTranslation();
@@ -15,7 +15,9 @@ const HeroSection = () => {
               style={{ textShadow: "2px 2px 20px rgba(255, 255, 255, 0.8)" }}
               className=" text-darkest-green font-bold text-[35px] md:text-h2-size lg:text-h1-size w-3/5"
             >
-              {t("Needing healthcare help at home can happen when you least expect it.")}
+              {t(
+                "Needing healthcare help at home can happen when you least expect it"
+              )}
             </h1>
             <OrangeButton variant="route" href="/marketplace" className="w-fit">
               {t("Find a Healthcare Provider")}
